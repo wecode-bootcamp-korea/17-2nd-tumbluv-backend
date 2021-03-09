7253e94688dfdc6589cd0fd46714324ebc30aa71
chaerin <cr434972@gmail.com>
2021-03-09 17:38:51 +0900

    CREATE: validatecodeview, unit test

--- user/tests.py
+++ user/tests.py
@@ -1,12 +1,14 @@
 import json
 import jwt
+import time
+import datetime
 
 from django.http   import JsonResponse
 from django.views  import View
 from django.test   import TestCase, Client
 from unittest.mock import patch, MagicMock
 
-from .models     import User
+from .models     import User, Verification
 from .views      import KakaoSignInView
 from my_settings import ALGORITHM, SECRET_KEY
 
@@ -65,3 +67,91 @@ class KakaoSignInTest(TestCase):
 
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json(), {'message': 'TOKEN_INVALID'})
+
+class TestValidateCodeView(TestCase):
+
+    def setUp(self):
+        Verification.objects.create(
+            email = 'test1234@test.com',
+            code  = '123456'
+        )
+    
+    def tearDown(self):
+        Verification.objects.all().delete()
+    
+    def test_validatecodeview_valid_code(self):
+        client = Client()
+        verification = {
+                'email': 'test1234@test.com',
+                'code' : '123456'
+                }
+        response = client.post('/user/signup/email-validation', json.dumps(verification), content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+                'message': 'EMAIL_VALIDATE_SUCCESS'
+                }
+        )
+
+    def test_validatecodeview_need_code(self):
+        client = Client()
+        verification = {
+                'email'  : 'test1234@test.com',
+                'number' : '123456'
+                }
+        response = client.post('/user/signup/email-validation', json.dumps(verification), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                'message': 'NEED_CODE'
+                }
+        )
+
+    def test_validatecodeview_invalid_code(self):
+        client = Client()
+        verification = {
+                'email': 'test1234@test.com',
+                'code' : '234567'
+                }
+        response = client.post('/user/signup/email-validation', json.dumps(verification), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                'message': 'INVALID_CODE'
+                }
+        )
+
+    def test_validatecodeview_time_out(self):
+        client = Client()
+        verification = {
+                'email': 'test1234@test.com',
+                'code' : '123456'
+                }
+        time.sleep(65)
+        response = client.post('/user/signup/email-validation', json.dumps(verification), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                'message': 'TIME_OUT'
+                }
+        )
+
+    def test_validatecodeview_time_in(self):
+        client = Client()
+        verification = {
+                'email': 'test1234@test.com',
+                'code' : '123456'
+                }
+        time.sleep(40)
+        response = client.post('/user/signup/email-validation', json.dumps(verification), content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            {
+                'message': 'EMAIL_VALIDATE_SUCCESS'
+                }
+        )
