876f814df8bc7390e1007dae83f3864a9aa82dcf
chaerin <cr434972@gmail.com>
2021-03-11 14:10:30 +0900

    FIX: user urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls    import path
 
-from user.views     import ValidateCodeView, KakaoSignInView, SignInView, SendMailView
+from user.views     import ValidateCodeView, KakaoSignInView, SignInView, SendMailView, SignUpView
 
 urlpatterns = [
     path('/signup/email-validation', ValidateCodeView.as_view()),
