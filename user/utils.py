2b7ab34ed7816b08e757ae4079ec6f9ffc552162
banana1019 <jyho930@gmail.com>
2021-03-11 13:53:25 +0900

    MODIFY: user/utils.py

--- user/utils.py
+++ user/utils.py
@@ -4,7 +4,7 @@ import json
 from django.http          import JsonResponse
 
 from .models              import User
-from my_settings          import SECRET, ALGORITHM
+from my_settings          import SECRET_KEY, ALGORITHM
 
 def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
@@ -12,7 +12,7 @@ def login_decorator(func):
             return JsonResponse({'message': 'NEED_LOGIN'}, status=401)
         try:
             access_token = request.headers['Authorization']
-            payload      = jwt.decode(access_token, SECRET, ALGORITHM)
+            payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
             login_user   = User.objects.get(id=payload['user_id'])
             request.user = login_user
             return func(self, request, *args, **kwargs)
@@ -21,3 +21,20 @@ def login_decorator(func):
         except User.DoesNotExist:
             return JsonResponse({'message': 'INVALID_USER'}, status=401)
     return wrapper
+
+    def user_decorator(func):
+        def wrapper(self, request, *args, **kwargs):
+            if 'Authorization' not in request.headers:
+                request.user = None
+                return func(self, request, *args, **kwargs)
+            try:
+                access_token = request.headers['Authorization']
+                payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
+                login_user   = User.objects.get(id=payload['user_id'])
+                request.user = login_user
+                return func(self, request, *args, **kwargs)
+            except jwt.DecodeError:
+                return JsonResponse({'message': 'INVALID_TOKEN'}, status=401)
+            except User.DoesNotExist:
+                return JsonResponse({'message': 'INVALID_USER'}, status=401)
+        return wrapper
